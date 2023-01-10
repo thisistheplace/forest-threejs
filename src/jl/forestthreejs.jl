9ff0d50d5c097c5420025f4f7a9eb1ed6a268b78
Ben Cannell <bencannell@bens-air.home>
2023-01-10 16:45:49 +0000

    Build updates

--- src/jl/forestthreejs.jl
+++ src/jl/forestthreejs.jl
@@ -10,11 +10,12 @@ A ForestThreejs component.
 Keyword arguments:
 - `id` (String; required)
 - `spacing` (Real; optional)
+- `stats` (Bool; optional)
 - `totalX` (Real; optional)
 - `totalZ` (Real; optional)
 """
 function forestthreejs(; kwargs...)
-        available_props = Symbol[:id, :spacing, :totalX, :totalZ]
+        available_props = Symbol[:id, :spacing, :stats, :totalX, :totalZ]
         wild_props = Symbol[]
         return Component("forestthreejs", "ForestThreejs", "forest_threejs", available_props, wild_props; kwargs...)
 end
